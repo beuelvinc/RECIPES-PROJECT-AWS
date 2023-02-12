c5bd15a06921f1f310017cb9f5394ca0c4861ef2
elvin <elvinc402@gmail.com>
2023-02-12 21:41:26 +0200

    #comment APIviews fixed #time 30 minutes

--- manage_foods/urls.py
+++ manage_foods/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path,re_path
 from .views import *
 
 urlpatterns = [
-    path('food/', FoodList.as_view(), name='food_list'),
-    path('food/<int:pk>/', FoodDetail.as_view(), name='food_detail'),
-    path('ingredient/', IngredientList.as_view(), name='food_list'),
-    path('ingredient/<int:pk>/', IngredientDetail.as_view(), name='food_detail'),
+    re_path(r'list/?$', FoodList.as_view(), name='food_list'),
+    path('detail/<int:pk>/', FoodDetail.as_view(), name='food_detail'),
+
+    re_path(r'ingredient/?$', IngredientList.as_view(), name='ingredient_list'),
+    path('ingredient/<int:pk>/', IngredientDetail.as_view(), name='ingredient_detail'),
+
 ]
